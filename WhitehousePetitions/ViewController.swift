2f6813d36433074c090135a70e2bb5be16fe9c78
Felipe Gil <felipe.gil@redspace.com>
2021-06-11 10:00:27 -0300

    first commit - skeleton

---
+++ WhitehousePetitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhitehousePetitions
+//
+//  Created by Felipe Gil on 2021-06-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
